1b7151ce4d1c0decd826359c8e5247913d254aa2
Isabel Henrique <isabel.henrique.dev@gmail.com>
2025-10-01 16:10:14 -0300

    criação do controller de professores

--- src/main/java/api/springsecurity/controller/TeacherController.java
+++ src/main/java/api/springsecurity/controller/TeacherController.java
@@ -1,17 +1,35 @@
 package api.springsecurity.controller;
 
+import api.springsecurity.dtos.student.StudentResponseDto;
+import api.springsecurity.dtos.teacher.TeacherRequestDto;
+import api.springsecurity.dtos.teacher.TeacherResponseDto;
+import api.springsecurity.service.TeacherService;
+import jakarta.servlet.ServletResponse;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("/teachers")
 public class TeacherController {
 
-    @PreAuthorize("hasRole('STUDENT')")
+    private final TeacherService teacherService;
+
+    @PreAuthorize("hasAnyRole('TEACHER', 'ADMIN')")
     @GetMapping
-    public String getTeacher() {
-        return "Teacher";
+    public ResponseEntity<Page<TeacherResponseDto>> findAll(Pageable pageable) {
+        Page<TeacherResponseDto> page = teacherService.findAllTeachers(pageable);
+        return ResponseEntity.ok(page);
+    }
+
+    @PostMapping
+    public ResponseEntity<TeacherResponseDto> createTeacher(@Valid @RequestBody TeacherRequestDto dto) {
+        TeacherResponseDto response = teacherService.create(dto);
+        return ResponseEntity.ok(response);
     }
 }
